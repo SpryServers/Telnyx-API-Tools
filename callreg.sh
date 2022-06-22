8565f00b86b0d0e8dd4261b59d811000781b1575
Tab Fitts <tfitts@spryservers.net>
2022-06-21 17:28:44 -0700

    Initial commit: Telnyx API curl tools

---
+++ callreg.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+unset APIKEY
+
+source api.config
+
+unset REGISTERCALL
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+      -r  | --regcall ) INPUT=1 REGISTERCALL=1 FROM=$2 TO=$3 REASON=$4
+      ;;
+      -gn | --get-numbers ) INPUT=1 && cat verifiedcalls/numbers.config
+      ;;
+      -gr | --get-reasons ) INPUT=1 && cat verifiedcalls/callreasons.config
+      ;;
+      -h | --help | \? ) INPUT=1 \
+      echo "Usage:"
+      echo ""
+      echo "--regcall <from-number> <to-number> \"<reason>\"    | -r  | Register a verified call"
+      echo "--get-numbers                                     | -gn | Get available call-from numbers"
+      echo "--get-reasons                                     | -gr | Get available call reasons"
+      echo "--help                                            | -h  | Print this help message"
+      exit 0
+      ;;
+    esac
+    shift
+  done
+
+
+call_register() {
+    curl -X POST https://api.telnyx.com/v2/calls/register \
+         -H "Content-Type: application/json" \
+         -H "Accept: application/json" \
+         -H "Authorization: Bearer $APIKEY" \
+         -d "{\"from\": \"$FROM\", \"to\": \"$TO\", \"reason\": \"$REASON\"}"
+}
+
+if [ -z $INPUT ]; then
+    echo "Empty or invalid parameter. Pass -h for help."
+    exit 1
+fi
+
+if [[ "$REGISTERCALL" -eq 1 ]]; then
+    if [ -z $APIKEY ]; then
+        echo "No API key specified in config. Please update api.config file."
+        exit 1
+    fi
+    call_register
+fi
