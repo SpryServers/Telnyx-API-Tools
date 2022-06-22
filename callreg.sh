86056ccca4c60d2251edcd3f8c442befb68fca18
Tab Fitts <tfitts@spryservers.net>
2022-06-22 10:37:50 -0700

    Call register script: Update config file references
    Signed-off-by: Tab Fitts <tfitts@spryservers.net>

--- callreg.sh
+++ callreg.sh
@@ -4,7 +4,7 @@ set -e
 
 unset APIKEY
 
-source api.config
+source api.conf
 
 unset REGISTERCALL
 
@@ -12,9 +12,9 @@ while [[ "$#" -gt 0 ]]; do
     case $1 in
       -r  | --regcall ) INPUT=1 REGISTERCALL=1 FROM=$2 TO=$3 REASON=$4
       ;;
-      -gn | --get-numbers ) INPUT=1 && cat verifiedcalls/numbers.config
+      -gn | --get-numbers ) INPUT=1 && cat verifiedcalls/numbers.conf
       ;;
-      -gr | --get-reasons ) INPUT=1 && cat verifiedcalls/callreasons.config
+      -gr | --get-reasons ) INPUT=1 && cat verifiedcalls/callreasons.conf
       ;;
       -h | --help | \? ) INPUT=1 \
       echo "Usage:"
@@ -45,7 +45,7 @@ fi
 
 if [[ "$REGISTERCALL" -eq 1 ]]; then
     if [ -z $APIKEY ]; then
-        echo "No API key specified in config. Please update api.config file."
+        echo "No API key specified in config. Please create and/or update api.conf file."
         exit 1
     fi
     call_register
