ecce984e44b504dfca3a7b2dec04b69c78edd936
Tab Fitts <tfitts@spryservers.net>
2022-06-22 15:00:07 -0700

    Add absolute directory detection for script/project directory
    Signed-off-by: Tab Fitts <tfitts@spryservers.net>

--- callreg.sh
+++ callreg.sh
@@ -3,18 +3,20 @@
 set -e
 
 unset APIKEY
-
-source api.conf
-
 unset REGISTERCALL
+
+SCRIPT_REL_DIR=$(dirname "${BASH_SOURCE[0]}")
+SCRIPT_DIR=$(cd $SCRIPT_REL_DIR && pwd)
+
+source $SCRIPT_DIR/api.conf
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
       -r  | --regcall ) INPUT=1 REGISTERCALL=1 FROM=$2 TO=$3 REASON=$4
       ;;
-      -gn | --get-numbers ) INPUT=1 && cat verifiedcalls/numbers.conf
+      -gn | --get-numbers ) INPUT=1 && cat $SCRIPT_DIR/verifiedcalls/numbers.conf
       ;;
-      -gr | --get-reasons ) INPUT=1 && cat verifiedcalls/callreasons.conf
+      -gr | --get-reasons ) INPUT=1 && cat $SCRIPT_DIR/verifiedcalls/callreasons.conf
       ;;
       -h | --help | \? ) INPUT=1 \
       echo "Usage:"
@@ -40,6 +42,7 @@ call_register() {
 
 if [ -z $INPUT ]; then
     echo "Empty or invalid parameter. Pass -h for help."
+    unset APIKEY
     exit 1
 fi
 
@@ -50,3 +53,4 @@ if [[ "$REGISTERCALL" -eq 1 ]]; then
     fi
     call_register
 fi
+unset APIKEY
